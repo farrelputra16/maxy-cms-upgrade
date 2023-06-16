5e0d2b90ff947bb00f06295e3986e935952f444c
Ardimas <dimasdsuprayogi@gmail.com>
2023-06-16 14:59:48 +0700

    delete current version

--- resources/views/dashboard/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.master')
-
-@section('title', 'Dashboard')
-
-@section('content')
-    <div style="padding: 12px;">
-       Sugeng Ravvuh, {{Auth::user()->name}}
-    </div>
-@endsection
