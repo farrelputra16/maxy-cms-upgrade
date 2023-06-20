8cb437a2ff9b1f85f1faab722eac483790024cfb
Ardimas <dimasdsuprayogi@gmail.com>
2023-06-20 10:18:42 +0700

    fix bad margin in navbar

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -17,41 +17,41 @@
             <div style="padding: 12px;">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <a class="navbar-brand" href="#">
-                        <img src="{{ asset('img/maxy_logo.jpg') }}" width="30" height="30" alt="">
+                        <img src="{{ asset('img/maxy_logo.jpg') }}" width="30" height="30" alt="" style="margin-left: 12px;">
                     </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
                     </button>
                   
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto">
                             <li class="nav-item">
                                 <a class="nav-link" href="{{route('getDashboard')}}">Dashboard<span class="sr-only"></span></a>
                             </li>
                             <li class="nav-item">
                                 <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            All Course
-                                        </a>
-                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                            <a class="dropdown-item" href="{{ route('getCourse') }}">Course</a>
-                                            <a class="dropdown-item" href="{{ route('getCourseModule') }}">Course Module</a>
-                                            <div class="dropdown-divider"></div>
-                                            <a class="dropdown-item" href="{{ route('getCourseClass') }}">Course Class</a>
-                                            <a class="dropdown-item" href="{{ route('getCourseClassMember') }}">Course Class Member</a>
-                                            <a class="dropdown-item" href="{{ route('getCourseClassModule') }}">Course Class Module</a>
-                                            <div class="dropdown-divider"></div>
-                                            <a class="dropdown-item" href="{{ route('getCoursePackage') }}">Course Package</a>
-                                            <a class="dropdown-item" href="{{ route('getCoursePackageBenefit') }}">Course Package Benefit</a>
-                                            <div class="dropdown-divider"></div>
-                                            <a class="dropdown-item" href="{{ route('getDifficulty')}}">Course Difficulty</a>
-                                            <div class="dropdown-divider"></div>
-                                            <a class="dropdown-item" href="#">Need Help?</a>
-                                        </div>
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        All Course
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('getCourse') }}">Course</a>
+                                        <a class="dropdown-item" href="{{ route('getCourseModule') }}">Course Module</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="{{ route('getCourseClass') }}">Course Class</a>
+                                        <a class="dropdown-item" href="{{ route('getCourseClassMember') }}">Course Class Member</a>
+                                        <a class="dropdown-item" href="{{ route('getCourseClassModule') }}">Course Class Module</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="{{ route('getCoursePackage') }}">Course Package</a>
+                                        <a class="dropdown-item" href="{{ route('getCoursePackageBenefit') }}">Course Package Benefit</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="{{ route('getDifficulty')}}">Course Difficulty</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#">Need Help?</a>
+                                    </div>
                                 </li>
                                 <li class="nav-item dropdown">
                                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        Manage User
+                                        User & Access
                                     </a>
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                         <a class="dropdown-item" href="{{ route('getUser') }}">Users</a>
@@ -79,7 +79,7 @@
                                 </li>
                             </li>
                         </ul>
-                        <form class="form-inline my-2 my-lg-0" method="post" action="{{route('logout')}}">
+                        <form class="form-inline my-2 my-lg-0 me-3" method="post" action="{{ route('logout') }}">
                             @csrf
                             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Logout</button>
                         </form>
