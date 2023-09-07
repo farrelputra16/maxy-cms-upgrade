ccc8e7dce72e823745b652ef3440bc8d20446df2
william.ch1029 <willam.ch1029@gmail.com>
2023-09-07 10:48:33 +0700

    Downgrade Laravel
    Downgrade Laravel8 PHP7.4.33

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Auth;
-use DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,18 +24,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-
-        view()->composer('*', function ($view){
-            if (Auth::check()){
-                $broGotAccessMaster = DB::table('access_group_detail')
-                    ->join('access_group', 'access_group_detail.access_group_id', '=', 'access_group.id')
-                    ->join('access_master', 'access_group_detail.access_master_id', '=', 'access_master.id')
-                    ->select('access_master.name')
-                    ->where('access_group.id', '=', Auth::user()->access_group_id)
-                    ->get();
-            
-                $view->with('broGotAccessMaster', $broGotAccessMaster );    
-            }
-        });  
     }
 }
