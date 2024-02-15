4c190d13264890da18d91711bdf2fd39a9606994
Hugo Hizkia <ryond.maxy.academy@gmail.com>
2024-02-15 16:13:37 +0700

    search by name untuk duplicate classs

--- Modules/ClassContentManagement/Resources/views/course_class/duplicate.blade.php
+++ Modules/ClassContentManagement/Resources/views/course_class/duplicate.blade.php
@@ -7,6 +7,21 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.bootstrap5.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+
+    <style>
+        .select2-container .select2-selection--single{
+            height:3vh !important;
+            width: 100% !important;
+        }
+        .select2-container--default .select2-selection--single{
+            border: 1px solid #ccc !important; 
+            border-radius: 3px !important; 
+        }
+    </style>
+
     <div style="padding: 12px 12px 0px 12px;">
         <h2>Duplicate Class</h2>
         <hr>
@@ -16,43 +31,42 @@
                 <div class="header" style="display: flex; justify-content: center; margin-bottom: 20px; background: #e0e0e0; padding: 10px 0px 10px 0px;">
                     <h5><b>Choose Class</b></h5>
                 </div>
-                <div class="five fields">
-                    <div class="field" style="width: 100%;">
-                        <label for="">Class</label>
-                        <select name="course_class_id" class="ui dropdown">
-                            @foreach ($class_list as $item)
-                                <option value="{{ $item->id }}">{{ $item->course_name }} Batch {{ $item->batch }}</option>
-                            @endforeach
-                        </select>
-                        @if ($errors->has('courseid'))
-                            @foreach ($errors->get('courseid') as $error)
-                                <span style="color: red;">{{$error}}</span>
-                            @endforeach
-                        @endif
-                    </div>
+                <div class="field">
+                    <label for="">Class</label>
+                    <select id="course_class" class="ui dropdown">
+                        <option value="">Select</option>
+                        @foreach ($class_list as $item)
+                            <option value="{{ $item->id }}">{{ $item->course_name }} Batch {{ $item->batch }}</option>
+                        @endforeach
+                    </select>
+                    @if ($errors->has('courseid'))
+                        @foreach ($errors->get('courseid') as $error)
+                            <span style="color: red;">{{$error}}</span>
+                        @endforeach
+                    @endif
                 </div>
-            
 
                 <div class="header" style="display: flex; justify-content: center; margin-bottom: 20px; background: #e0e0e0; padding: 10px 0px 10px 0px; margin-top: 50px;">
                     <h5><b>New Class</b></h5>
                 </div>
-                <div class="five fields">
-                    <div class="field" style="width: 50%;">
-                        <label for="">Course</label>
-                        <select name="course_id" class="ui dropdown">
+                <div class="two fields">
+                    <div class="field">
+                        <label for="course_id">Course</label>
+                        <select id="course_id" name="course_id" class="form-control select2">
+                            <option value="">Select</option>
                             @foreach ($course_list as $item)
                                 <option value="{{ $item->id }}">{{ $item->name }}</option>
                             @endforeach
                         </select>
-                        @if ($errors->has('courseid'))
-                            @foreach ($errors->get('courseid') as $error)
+                        @if ($errors->has('course_id'))
+                            @foreach ($errors->get('course_id') as $error)
                                 <span style="color: red;">{{$error}}</span>
                             @endforeach
                         @endif
                     </div>
-                    <div class="field" style="width: 50%;">
+                    <div class="field">
                         <label for="">Batch</label>
-                        <input type="number" name="batch" placeholder="Masukkan Batch">
+                        <input type="number" name="batch" placeholder="Masukkan Batch" style="height:3vh">
                         @if ($errors->has('batch'))
                             @foreach ($errors->get('batch') as $error)
                                 <span style="color: red;">{{$error}}</span>
@@ -72,4 +86,25 @@
         </form>
         <a href="{{ route("getCourseClass") }}"><button class=" right floated ui red button">Batal</button></a>
     </div>
+
+    
+    <script>
+    $(document).ready(function() {
+        $('#course_class').select2({
+            placeholder: "Search for a class",
+            allowClear: true
+        });
+    });
+    </script>
+
+    <script>
+        $(document).ready(function() {
+            $('#course_id').select2({
+                placeholder: "Search for a course",
+                allowClear: true
+            });
+        });
+    </script>
+
+    
 @endsection
