41df771ddeb3dd4d2cf7b7fa461d0ac857d0ad5a
Hugo Hizkia <ryond.maxy.academy@gmail.com>
2024-02-15 10:08:53 +0700

    betulin index.php table untuk child course class module

--- Modules/ClassContentManagement/Resources/views/course_class_module/child/index.blade.php
+++ Modules/ClassContentManagement/Resources/views/course_class_module/child/index.blade.php
@@ -4,68 +4,135 @@
 
 @section('content')
     <div style="padding: 0px 12px 0px 12px;">
-        <hr style="margin-bottom: 0px;">
-        <h2 class="ui center aligned header">
-            {{ $courseParent->courseModule->name }}
-            <div class="sub header">Manage your course child settings.</div>
-        </h2>
-        <nav class="navbar bg-body-tertiary" style="padding: 12px 0px 12px 0px;">
-            <div class="navbar-nav">
-                <a class="btn btn-primary"
+        <!DOCTYPE html>
+        <html>
+
+        <head>
+            <title>Course Class Child Module</title>
+            <!-- Include CSS libraries for styling the table -->
+            <link rel="stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+            <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.bootstrap5.min.css">
+            <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+
+        </head>
+
+        <body>
+            <h2>Course Class Child Module</h2>
+            <hr>
+            <div class="ui breadcrumb pt-2 pb-4">
+            <a class="section" href="{{ url('/') }}">Dashboard</a>
+            <i class="right angle icon divider"></i>
+            <div class="active section">Course Class Child Module</div>
+        </div>
+            <div id="example_wrapper">
+                <div class="navbar bg-body-tertiary" style="padding: 12px 0px 12px 0px;">
+                    <div class="navbar-nav">
+                    <a class="btn btn-primary"
                     href="{{ route('getAddCourseClassChildModule', ['id' => $courseParent->id, 'course_class_id' => $courseParent->course_class_id]) }}"
                     role="button">Tambah Child Module +</a>
-            </div>
-        </nav>
-        <div id="table_content">
-            <table class="ui tablet unstackable table">
-                <thead style="text-align: center; vertical-align: middle;">
-                    <tr>
-                        <th>ID</th>
-                        <th>Start Date</th>
-                        <th>End Date</th>
-                        <th>Module Name</th>
-                        <th>Module Priority</th>
-                        <th>Module Level</th>
-                        <th>Content</th>
-                        <th>Description</th>
-                        <th>Created At</th>
-                        <th>Created By</th>
-                        <th>Updated At</th>
-                        <th>Updated By</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody style="text-align: center; vertical-align: middle;">
-                    @foreach ($courseClassChildModule as $item)
+                    </div>
+                </div>
+                <table id="example" class="table table-striped" style="width:100%">
+                    <thead>
                         <tr>
-                            <td>{{ $item->id }}</td>
-                            <td>{{ $item->start_date }}</td>
-                            <td>{{ $item->end_date }}</td>
-                            <td>{{ $item->courseModule->name }}</td>
-                            <td>{{ $item->courseModule->priority }}</td>
-                            <td>{{ $item->courseModule->level }}</td>
-                            <td>{{ $item->courseModule->content ?? '-' }}</td>
-                            <td id="description">{!! !empty($item->courseModule->description) ? \Str::limit($item->courseModule->description, 30) : '-' !!}</td>
-                            <td>{{ $item->courseModule->created_at }}</td>
-                            <td>{{ $item->userCreated->name }}</td>
-                            <td>{{ $item->courseModule->updated_at }}</td>
-                            <td>{{ $item->userUpdated->name }}</td>
-                            <td>
-                                @if ($item->status == 1)
-                                    <a class="ui tiny green label" style="text-decoration: none;">Aktif</a>
-                                @else
-                                    <a class="ui tiny red label" style="text-decoration: none;">Non Aktif</a>
-                                @endif
-                            </td>
-                            <td>
-                                <a href="{{ route('getEditCourseClassChildModule', ['id' => $item->id, 'parent_id' => $courseParent->id]) }}"
-                                    style="text-decoration: none; color:blue;">Edit</a>
-                            </td>
+                            <th>ID</th>
+                            <th>Start Date</th>
+                            <th>End Date</th>
+                            <th>Module Name</th>
+                            <th>Module Priority</th>
+                            <th>Module Level</th>
+                            <th>Content</th>
+                            <th>Description</th>
+                            <th>Created At</th>
+                            <th>Created By</th>
+                            <th>Updated At</th>
+                            <th>Updated By</th>
+                            <th>Status</th>
+                            <th>Action</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        @foreach ($courseClassChildModule as $item)
+                            <tr>
+                                <td>{{ $item->id }}</td>
+                                <td>{{ $item->start_date }}</td>
+                                <td>{{ $item->end_date }}</td>
+                                <td>{{ $item->courseModule->name }}</td>
+                                <td>{{ $item->courseModule->priority }}</td>
+                                <td>{{ $item->courseModule->level }}</td>
+                                <td>{{ $item->courseModule->content ?? '-' }}</td>
+                                <td id="description">{!! !empty($item->courseModule->description) ? \Str::limit($item->courseModule->description, 30) : '-' !!}</td>
+                                <td>{{ $item->courseModule->created_at }}</td>
+                                <td>{{ $item->userCreated->name }}</td>
+                                <td>{{ $item->courseModule->updated_at }}</td>
+                                <td>{{ $item->userUpdated->name }}</td>
+                                <td>
+                                    @if ($item->status == 1)
+                                        <a class="ui tiny green label" style="text-decoration: none;">Aktif</a>
+                                    @else
+                                        <a class="ui tiny red label" style="text-decoration: none;">Non Aktif</a>
+                                    @endif
+                                </td>
+                                <td>
+                                    <a href="{{ route('getEditCourseClassChildModule', ['id' => $item->id, 'parent_id' => $courseParent->id]) }}"
+                                        style="text-decoration: none; color:blue;">Edit</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+            <!-- Include JS libraries for DataTable initialization -->
+            <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+            <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+            <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+            <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+            <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.bootstrap5.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+            <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+            <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+            <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.colVis.min.js"></script>
+
+            
+            <script>
+                $(document).ready(function () {
+                    let table = $('#example').DataTable({
+                        lengthChange: true,
+                        lengthMenu: [10, 25, 50, 100],
+                        buttons: ['copy', 'excel', 'pdf', 'colvis'],
+                        searching: true,
+                    });
+
+                    // Add individual column search inputs and titles
+                    $('#example thead th').each(function () {
+                        let title = $(this).text();
+                        $(this).html('<div class="text-center">' + title +
+                    '</div><div class="mt-2"><input class="form-control" type="text" placeholder="Search ' + title +
+                    '" /></div>');
+                    });
+
+                    // Apply individual column search
+                    table.columns().every(function () {
+                        let that = this;
+                        $('input', this.header()).on('keyup change', function () {
+                            if (that.search() !== this.value) {
+                                that.search(this.value).draw();
+                            }
+                        });
+                    });
+
+                    table.buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
+                });
+            </script>
+
+        </body>
+
+        </html>
+
     </div>
 @endsection
