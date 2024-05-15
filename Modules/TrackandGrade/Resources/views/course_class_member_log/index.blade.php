8384d452de7566bc7922754592aad3bb744f26c1
Hugo Hizkia <ryond.maxy.academy@gmail.com>
2024-05-15 08:34:20 +0700

    Tracking

--- Modules/TrackandGrade/Resources/views/course_class_member_log/index.blade.php
+++ Modules/TrackandGrade/Resources/views/course_class_member_log/index.blade.php
@@ -1,6 +1,6 @@
 @extends ('layout.master')
 
-@section('title', 'Voucher')
+@section('title', 'History')
 @section('content')
 
     <head>
--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -10,8 +10,7 @@
         <head>
             <title>Course</title>
             <!-- Include CSS libraries for styling the table -->
-            <link rel="stylesheet"
-                href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
             <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
             <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.bootstrap5.min.css">
             <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
