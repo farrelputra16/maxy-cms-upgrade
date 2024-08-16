5d0eabee1f5d786359e04152fe55be6901a8b83d
kristiardy <kristiardy.maxy.academy@gmail.com>
2024-08-16 08:52:39 +0700

    perbaikan tampilan survey result detail
    perbaikan tampilan survey result detail

--- resources/views/m_survey/result/detail.blade.php
+++ resources/views/m_survey/result/detail.blade.php
@@ -1,173 +1,43 @@
 @extends('layout.master')
 
-@section('title', 'Detail Survey Result')
+@section('title', 'Survey Result Detil')
 
 @section('content')
-<!DOCTYPE html>
-<html>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+    <div style="padding: 0px 12px 0px 12px;">
+        <h2>Survey Result Detil</h2>
+        <hr style="padding-bottom:1%">
+        <form class="ui form">
+            <div class="field">
+                <div class="field">
+                    <label for="">Survey</label>
+                    <input type="text" value="{{ $currentData->MSurvey->name }}" disabled style='background-color: white;'>
+                </div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detail Survey Result</title>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+                <div class="field">
+                    <label for="">Responden</label>
+                    <input type="text" value="{{ $currentData->User->name }}" disabled style='background-color: white;'>
+                </div>
 
-    <style>
-        body {
-            background-color: #E3E5EE;
-        }
-        
-        .conTitle {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            margin-top: 2rem;
-        }
+                <div class="field">
+                    <label for="">Content</label>
+                    <textarea disabled>{{ $currentData->content }}</textarea>
+                </div>
 
-        .h2 {
-            font-weight: bold;
-            color: #232E66;
-            padding-left: .1rem;
-            font-size: 22px;
-            margin-bottom: -0.5rem;
-            margin-left: 1rem;
-        }
+                <div class="field">
+                    <label for="">Score</label>
+                    <input type="text" value="{{ $currentData->score }}" disabled style='background-color: white;'>
+                </div>
 
-        .btnlogout {
-            margin-right: 2rem;
-            margin-bottom: .5rem;
-            background-color: #FBB041;
-            color: #FFF;
-            width: 80px;
-            height: 35px;
-            border-radius: 10px;
-            border: none;
-            box-shadow: none;
-            font-weight: bold;
-        }
-
-        .breadcrumb {
-            border-top: 2px solid black;
-            display: inline-block;
-            width: 97%;;
-            margin-left: 1rem;
-            margin-bottom: 1rem;
-        }
-
-        .breadcrumb .sectionDashboard,
-        .breadcrumb .divider,
-        .breadcrumb .sectionMaster,
-        .breadcrumb .sectionProposal {
-            display: inline;
-            font-size: 11px;
-            font-weight: bold;
-        }
-
-        .breadcrumb .divider {
-            margin: 0 5px;
-        }
-
-        .container-form {
-            margin-top: 2rem;
-        }
-
-        .field {
-            margin-bottom: 1rem;
-        }
-
-        .right-button {
-            float: right;
-        }
-
-        .btnBatal {
-            background-color: #F13C20;
-            color: #FFF;
-            color: #FFF;
-            width: 80px;
-            height: 35px;
-            border-radius: 10px;
-            border: none;
-            box-shadow: none;
-            font-weight: bold;
-        }
-
-        .btnSave {
-            background-color: #4056A1;
-            color: #FFF;
-            color: #FFF;
-            width: 130px;
-            height: 35px;
-            border-radius: 10px;
-            border: none;
-            box-shadow: none;
-            font-weight: bold;
-        }
-
-        .divBatal {
-            text-align: right;
-            margin-right: 10rem;
-            margin-bottom: 1rem;
-            margin-top: -3rem;
-        }
-
-        .divSave {
-            text-align: right;
-            margin-right: 1rem;
-            margin-bottom: .5rem;
-            margin-left: 65rem;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="container conTitle">
-        <h2 class="h2">Edit Survey</h2>
-        <form class="form-inline my-2 my-lg-0 me-3" method="post" action="{{ route('logout') }}">
-            @csrf
-            <button class="btnlogout" type="submit">Logout</button>
+                <div class="field">
+                    <label for="">Created At</label>
+                    <input type="text" value="{{ $currentData->created_at }}" disabled style='background-color: white;'>
+                </div>
+            </div>
         </form>
     </div>
-
-    <div class="breadcrumb pt-2 pb-4">
-        <a class="sectionDashboard" href="{{ url('/') }}">Dashboard</a>
-        <span class="divider">></span>
-        <div class="sectionMaster">Master</div>
-        <span class="divider">></span>
-        <div class="sectionProposal">Survey</div>
-        <span class="divider">></span>
-        <div class="sectionProposal">Survey Result</div>
-    </div>
-
-    <div class="container">
-        <div class="field">
-            <label for="">Survey</label>
-            <p>{{ $currentData->MSurvey->name }}</p>
-        </div>
-
-        <div class="field">
-            <label for="">Responden</label>
-            <p>{{ $currentData->User->name }}</p>
-        </div>
-
-        <div class="field">
-            <label for="">Content</label>
-            <p>{{ $currentData->content }}</p>
-        </div>
-
-        <div class="field">
-            <label for="">Score</label>
-            <p>{{ $currentData->score }}</p>
-        </div>
-
-        <div class="field">
-            <label for="">Created At</label>
-            <p>{{ $currentData->created_at }}</p>
-        </div>
-    </div>
-</body>
-
-<script>
-    
-</script>
-</html>
+    <script>
+        CKEDITOR.replace('description');
+        CKEDITOR.replace('content');
+    </script>
 @endsection
