1610760f57373c914c1d8958e1a7210d2ee92859
Bimo Alfarrabi <william.maxy.academy@gmail.com>
2024-10-07 16:38:38 +0700

    CMS: UI V3: Transkrip: Migrate UI to V3

---
+++ resources/views/transkrip/indexv3.blade.php
@@ -0,0 +1,99 @@
+@extends('layout.main-v3')
+
+@section('title', 'Transkrip')
+
+@section('content')
+    <!-- Begin Page Title -->
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                <h4 class="mb-sm-0 font-size-18">Data Overview</h4>
+
+                <!-- Begin Breadcrumb -->
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item"><a href="javascript:void(0);">Master</a></li>
+                        <li class="breadcrumb-item"><a>Members</a></li>
+                        <li class="breadcrumb-item active">Transkrip</li>
+                    </ol>
+                </div>
+                <!-- End Breadcrumb -->
+            </div>
+        </div>
+    </div>
+    <!-- End Page Title -->
+
+    <!-- Begin Content -->
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Transkip</h4>
+                    <p class="card-title-desc">
+                        This page presents a comprehensive overview of all available data, displayed in an interactive
+                        and sortable DataTable format. Each row represents a unique data, providing key details such as
+                        name, description, and status. Utilize the <b>column visibility, sorting, and column search bar</b>
+                        features to
+                        customize your view and quickly access the specific information you need.
+                    </p>
+
+                    <table id="datatable" class="table table-bordered dt-responsive nowrap w-100"
+                        data-colvis="[1, 6, 7, 8, 9]">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Id</th>
+                                <th class="data-medium">Student</th>
+                                <th>Period</th>
+                                <th>Course Class</th>
+                                <th>Score</th>
+                                <th>Created At</th>
+                                <th>Created Id</th>
+                                <th>Updated At</th>
+                                <th>Updated Id</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($data as $key => $item)
+                                <tr>
+                                    <td>{{ $key + 1 }}</td>
+                                    <td>{{ $item->id }}</td>
+                                    <td class="data-medium" data-toggle="tooltip" data-placement="top"
+                                        title="{{ $item->User->name }}">
+                                        {!! \Str::limit($item->User->name, 30) !!}
+                                    </td>
+                                    <td>{{ $item->CourseClass->Schedule->first()->MAcademicPeriod->name }}</td>
+                                    <td>{{ $item->CourseClass->slug }}</td>
+                                    <td>{{ $item->MScore->name }}</td>
+                                    <td>{{ $item->created_at }}</td>
+                                    <td>{{ $item->created_id }}</td>
+                                    <td>{{ $item->updated_at }}</td>
+                                    <td>{{ $item->updated_id }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>No</th>
+                                <th>Id</th>
+                                <th class="data-medium">Student</th>
+                                <th>Period</th>
+                                <th>Course Class</th>
+                                <th>Score</th>
+                                <th>Created At</th>
+                                <th>Created Id</th>
+                                <th>Updated At</th>
+                                <th>Updated Id</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Content -->
+@endsection
+
+@section('script')
+
+@endsection
