1f8463fe735dd9bb24143dcb94851d990d534c3a
Bimo Alfarrabi <william.maxy.academy@gmail.com>
2024-10-09 15:54:49 +0700

    CMS: UI V3: Proposals: Fix typo

--- resources/views/m_proposal_type/editv3.blade.php
+++ resources/views/m_proposal_type/editv3.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-12">
             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                <h4 class="mb-sm-0 font-size-18">Add New Data</h4>
+                <h4 class="mb-sm-0 font-size-18">Edit Data</h4>
 
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
@@ -27,7 +27,7 @@
             <div class="card">
                 <div class="card-body">
 
-                    <h4 class="card-title">Edit Proposal Type</h4>
+                    <h4 class="card-title">Edit: {{ $currentData->name }}</h4>
                     <p class="card-title-desc">This page allows you to update a data's information by modifying the data
                         listed below. Ensure that all the information you enter is accurate to provide the best learning
                         experience for the course participants.</p>
