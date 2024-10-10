d7dcfc197d37a6a9d169db76aabbb656a6c4dad8
abdul <abdulhakim54362@gmail.com>
2024-10-10 10:45:34 +0700

    FIX: Link Logo
    Merubah tujuan href pada logo

--- resources/views/partials/top-navbar.blade.php
+++ resources/views/partials/top-navbar.blade.php
@@ -3,7 +3,7 @@
         <div class="d-flex">
             <!-- LOGO -->
             <div class="navbar-brand-box">
-                <a href="index.html" class="logo logo-dark">
+                <a href="{{ route('getDashboard') }}" class="logo logo-dark">
                     <span class="logo-sm">
                         <img src="{{ asset('jago-digital/assets/images/logo-m.png') }}" alt="" height="22">
                     </span>
@@ -17,7 +17,7 @@
                     </span>
                 </a>
 
-                <a href="index.html" class="logo logo-light">
+                <a href="{{ route('getDashboard') }}" class="logo logo-light">
                     <span class="logo-sm">
                         <img src="{{ asset('jago-digital/assets/images/logo-m.png') }}" alt="" height="22">
                     </span>
