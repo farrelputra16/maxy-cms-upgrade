b4fa718ebc295c3dd9f9ac01fc8152981aa4d4d3
Bimo Alfarrabi <william.maxy.academy@gmail.com>
2024-10-31 15:16:23 +0700

    feat: new design

--- resources/views/bad_access.blade.php
+++ resources/views/bad_access.blade.php
@@ -1,17 +1,78 @@
-@extends('layout.master')
+<!doctype html>
+<html lang="en">
 
-@section('title', 'BAD ACCESS')
+<head>
+    <meta charset="utf-8">
 
-@section('content')
-    <div style="padding: 0px 12px 0px 12px;">
-        <div class="ui icon negative message">
-            <i class="asterisk loading icon"></i>
-            <div class="content">
-                <div class="header">
-                    PDI "Partai Demokrasi Indonesia"
+    <title>No Access</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description">
+    <meta name="keywords" content="veltrix,veltrix laravel,admin template,new admin panel,laravel 10">
+    <meta content="Themesbrand" name="author">
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="/assets/images/favicon.ico">
+
+    <!-- Bootstrap Css -->
+    <link href="/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css">
+    <!-- Icons Css -->
+    <link href="/assets/css/icons.min.css" rel="stylesheet" type="text/css">
+    <!-- Fontawesome Icons -->
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+
+<body>
+    <!-- Start content -->
+
+    <div class="authentication-bg d-flex align-items-center pb-0 vh-100">
+        <div class="content-center w-100">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-10">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row align-items-center">
+                                    <div class="col-lg-4 ms-auto">
+                                        <div class="ex-page-content">
+                                            <h1 class="text-dark display-1 mt-4">403!</h1>
+                                            <h4 class="mb-4">Sorry, you don't have permission to access this page!
+                                            </h4>
+                                            <button class="btn btn-primary mb-5 waves-effect waves-light"
+                                                onclick="window.history.back()"><i class="fas fa-arrow-left me-2"></i>Go
+                                                Back</button>
+                                        </div>
+
+                                    </div>
+                                    <div class="col-lg-5 mx-auto">
+                                        <img src="http://127.0.0.1:8000/assets/images/error.png" alt=""
+                                            class="img-fluid mx-auto d-block">
+                                    </div>
+                                </div>
+                            </div><!-- end cardbody -->
+                        </div><!-- end card -->
+                    </div><!-- end col -->
                 </div>
-                <p>Anda berada dalam zona perbatasan, mohon segera kembali</p>
+                <!--end row-->
             </div>
+            <!-- end container -->
         </div>
+
     </div>
-@endsection
+
+    <!-- content -->
+
+    <!-- JAVASCRIPT -->
+    <script src="http://127.0.0.1:8000/assets/libs/jquery/jquery.min.js"></script>
+    <script src="http://127.0.0.1:8000/assets/libs/bootstrap/bootstrap.min.js"></script>
+    <script src="http://127.0.0.1:8000/assets/libs/metismenu/metismenu.min.js"></script>
+    <script src="http://127.0.0.1:8000/assets/libs/simplebar/simplebar.min.js"></script>
+    <script src="http://127.0.0.1:8000/assets/libs/node-waves/node-waves.min.js"></script>
+
+    <!-- onother scripts -->
+
+    <script src="http://127.0.0.1:8000/assets/js/app.js"></script>
+
+
+</body>
+
+</html>
