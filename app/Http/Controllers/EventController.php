3c46617ca12f147266de8e8c790d9a6eb612adc5
abdul <abdulhakim54362@gmail.com>
2024-11-07 08:26:17 +0700

    FIX: Date Validation On Events

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -39,7 +39,7 @@ class EventController extends Controller
         $validated = $request->validate([
             'name' => 'required',
             'date_start' => 'required|date|after_or_equal:now',
-            'date_end' => 'required|date|after_or_equal:now',
+            'date_end' => 'required|date|after:date_start',
             'image' => 'required',
             'url' => 'nullable|url',
         ]);
@@ -100,8 +100,8 @@ class EventController extends Controller
 
         $validated = $request->validate([
             'name' => 'required',
-            'date_start' => 'required|date|after_or_equal:now',
-            'date_end' => 'required|date|after_or_equal:now',
+            'date_start' => 'required|date',
+            'date_end' => 'required|date|after:date_start',
             'url' => 'nullable|url',
         ]);
 
