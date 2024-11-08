08c591cb6e604fb47e1cbd69a44cec9cee1d985f
Bimo Alfarrabi <william.maxy.academy@gmail.com>
2024-11-08 10:35:03 +0700

    Revert "fix: change id in order to use TinyMCE textbox"
    This reverts commit f232edb61476f713c5285fac0dd4a9e222b269b9.

--- resources/views/course/editv3.blade.php
+++ resources/views/course/editv3.blade.php
@@ -229,7 +229,7 @@
                         <div class="mb-3 row">
                             <label for="input-content" class="col-md-2 col-form-label">Description</label>
                             <div class="col-md-10">
-                                <textarea id="elm1" name="description" class="form-control">{{ old('description', $courses->description) }}</textarea>
+                                <textarea id="elm3" name="description" class="form-control">{{ old('description', $courses->description) }}</textarea>
                             </div>
                         </div>
                         <div class="row form-switch form-switch-md mb-3 p-0" dir="ltr">
