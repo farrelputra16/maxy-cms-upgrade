60482d8fa38ff002d51ebb2f372fa7b5d5dd5b14
Bimo Alfarrabi <william.maxy.academy@gmail.com>
2024-11-13 13:56:04 +0700

    fix: ux writing missed menu

--- resources/views/partials/side-navbar.blade.php
+++ resources/views/partials/side-navbar.blade.php
@@ -133,7 +133,7 @@
                     <li>
                         <a href="javascript: void(0);" class="has-arrow waves-effect">
                             <i class='bx bx-cog'></i>
-                            <span key="t-setting">Settings</span>
+                            <span key="t-setting">Pengaturan</span>
                         </a>
                         <ul class="sub-menu" aria-expanded="false">
                             <li><a href="{{ route('getGeneral') }}" key="t-general">Pengaturan Umum</a></li>
