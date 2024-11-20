bf30e53d7ed40abf19f5c0d7e48b2353fafd2d57
dull <william.maxy.academy@gmail.com>
2024-11-20 15:47:14 +0700

    fix: ux writing 'Index & Edit Category'
    fix: ux writing 'Index & Edit Category'

--- resources/views/category/editv3.blade.php
+++ resources/views/category/editv3.blade.php
@@ -1,19 +1,19 @@
 @extends('layout.main-v3')
 
-@section('title', 'Edit Program Studi')
+@section('title', 'Ubah Program Studi')
 
 @section('content')
     <!-- Judul Halaman -->
     <div class="row">
         <div class="col-12">
             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                <h4 class="mb-sm-0 font-size-18">Edit Data</h4>
+                <h4 class="mb-sm-0 font-size-18">Ubah Data</h4>
 
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
                         <li class="breadcrumb-item"><a href="javascript: void(0);">Master</a></li>
                         <li class="breadcrumb-item"><a href="{{ route('getCategory') }}">Program Studi</a></li>
-                        <li class="breadcrumb-item active">Edit Program Studi</li>
+                        <li class="breadcrumb-item active">Ubah Program Studi</li>
                     </ol>
                 </div>
 
@@ -27,7 +27,7 @@
             <div class="card">
                 <div class="card-body">
 
-                    <h4 class="card-title">Edit: {{ $Category->name }}</h4>
+                    <h4 class="card-title">Ubah: {{ $Category->name }}</h4>
                     <p class="card-title-desc">
                         Di halaman ini, Anda dapat memperbarui data program studi yang ada dengan mengisi atau mengedit informasi
                         di bawah ini. Setelah selesai, klik "Simpan Perubahan" untuk memperbarui data program studi.
