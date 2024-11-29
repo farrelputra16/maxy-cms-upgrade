36f3af31a04a36c09b87959c6e530bb0fae171cd
Abdul <abdulhakim54362@gmail.com>
2024-11-29 14:25:29 +0700

    FIX: Breadcrumb On Proposal and Transkrip

--- resources/views/proposal/indexv3.blade.php
+++ resources/views/proposal/indexv3.blade.php
@@ -13,7 +13,7 @@
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
                         <li class="breadcrumb-item"><a href="javascript:void(0);">Master</a></li>
-                        <li class="breadcrumb-item"><a>Members</a></li>
+                        <li class="breadcrumb-item"><a>Mahasiswa</a></li>
                         <li class="breadcrumb-item active">Proposals</li>
                     </ol>
                 </div>
