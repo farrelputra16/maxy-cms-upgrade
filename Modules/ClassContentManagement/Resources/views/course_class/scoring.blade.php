503bf8653bcffc8288dc1b6892a6ffee07def76e
febriandfa <febriandaffa123@gmail.com>
2024-12-05 14:45:17 +0700

    fix:bread crumb course class management scoring

--- Modules/ClassContentManagement/Resources/views/course_class/scoring.blade.php
+++ Modules/ClassContentManagement/Resources/views/course_class/scoring.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layout.main-v3')
 
 @section('title', 'Ubah Penilaian')
@@ -12,8 +11,10 @@
 
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
-                        <li class="breadcrumb-item"><a href="{{ route('getCourseClass') }}">Class</a></li>
-                        <li class="breadcrumb-item active">Ubah Penilaian: {{ $id }}</li>
+                        <li class="breadcrumb-item"><a href="javascript: void(0);">Master</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('getCourseClass') }}">Daftar Kelas</a></li>
+                        <li class="breadcrumb-item active">Ubah Penilaian Kelas:
+                            {{ $classes[0]->CourseClass->course->name }} {{ $classes[0]->CourseClass->batch }}</li>
                     </ol>
                 </div>
 
@@ -27,41 +28,47 @@
             <div class="card">
                 <div class="card-body">
 
-                    <h4 class="card-title">{{ $classes[0]->CourseClass->slug }} <small>[ ID: {{ $id }} ]</small></h4>
+                    <h4 class="card-title">{{ $classes[0]->CourseClass->slug }} <small>[ ID: {{ $id }} ]</small>
+                    </h4>
                     <p class="card-title-desc">
-                        Halaman ini memungkinkan Anda untuk memperbarui informasi penilaian dari kelas yang dipilih. 
+                        Halaman ini memungkinkan Anda untuk memperbarui informasi penilaian dari kelas yang dipilih.
                         Pastikan semua data yang dimasukkan akurat agar pengalaman belajar peserta tetap optimal.
-                    <br><br>
-                    <strong>Cara Penggunaan:</strong>
+                        <br><br>
+                        <strong>Cara Penggunaan:</strong>
                     <ul>
                         <li>Isi kolom <strong>Kehadiran</strong> dengan persentase kehadiran peserta di kelas.</li>
                         <li>Isi kolom <strong>Nilai Modul</strong> sesuai dengan nilai masing-masing modul.</li>
-                        <li>Setelah semua detail selesai diisi, klik tombol <strong>'Simpan & Perbarui'</strong> untuk memperbarui data penilaian.</li>
+                        <li>Setelah semua detail selesai diisi, klik tombol <strong>'Simpan & Perbarui'</strong> untuk
+                            memperbarui data penilaian.</li>
                     </ul>
                     </p>
 
-                    <form id="addCourseClassScoring" action="{{ route('postCourseClassScoring', ['id' => $id]) }}" method="post"
-                        enctype="multipart/form-data">
+                    <form id="addCourseClassScoring" action="{{ route('postCourseClassScoring', ['id' => $id]) }}"
+                        method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="mb-3 row">
                             <label for="input-title" class="col-md-2 col-form-label">Kehadiran</label>
                             <div class="col-md-10">
-                                <input class="form-control" type="number" name="attendance" value="{{ $classes[0]->CourseClass->percentage }}" id="input-title">
+                                <input class="form-control" type="number" name="attendance"
+                                    value="{{ $classes[0]->CourseClass->percentage }}" id="input-title">
                             </div>
                         </div>
 
-                        @foreach($classes as $item)
-                        <div class="mb-3 row">
-                            <label for="input-member" class="col-md-2 col-form-label">{{ $item->CourseModule->name }}</label>
-                            <div class="col-md-10">
-                                <input class="form-control" type="number" name="{{ $item->id }}" value="{{ $item->percentage }}" id="input-member">
+                        @foreach ($classes as $item)
+                            <div class="mb-3 row">
+                                <label for="input-member"
+                                    class="col-md-2 col-form-label">{{ $item->CourseModule->name }}</label>
+                                <div class="col-md-10">
+                                    <input class="form-control" type="number" name="{{ $item->id }}"
+                                        value="{{ $item->percentage }}" id="input-member">
+                                </div>
                             </div>
-                        </div>
                         @endforeach
 
                         <div class="mb-3 row justify-content-end">
                             <div class="text-end">
-                                <button type="submit" class="btn custom-btn-submit btn-primary w-md text-center" form="addCourseClassScoring">Simpan & Perbarui</button>
+                                <button type="submit" class="btn custom-btn-submit btn-primary w-md text-center"
+                                    form="addCourseClassScoring">Simpan & Perbarui</button>
                             </div>
                         </div>
                     </form>
@@ -73,7 +80,5 @@
 @endsection
 
 @section('script')
-    <script>
-        
-    </script>
+    <script></script>
 @endsection
