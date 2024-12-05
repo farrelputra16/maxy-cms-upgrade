dc5e1d8ca94f32821d775b48166ab589047ad606
candra maxy <william.maxy.academy@gmail.com>
2024-12-05 13:37:16 +0700

    fix: middleware template_certificate

--- Modules/CertificateTemplate/Routes/web.php
+++ Modules/CertificateTemplate/Routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::group(['prefix' => 'course/class', 'middleware' => ['access:certificate_template_manage']], function () {
+Route::group(['prefix' => 'course/class', 'middleware' => ['access:template_certificate_manage']], function () {
     Route::resource('certificate-templates', "CertificateTemplateController");
 });
