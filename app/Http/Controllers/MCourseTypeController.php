90db3125537f343f201ed2c631d4c9e084dff2be
Abdul <abdulhakim54362@gmail.com>
2024-12-17 13:27:00 +0700

    Sorting For Table Course Type and Diff Type

--- app/Http/Controllers/MCourseTypeController.php
+++ app/Http/Controllers/MCourseTypeController.php
@@ -18,8 +18,8 @@ class MCourseTypeController extends Controller
     function getMCourseTypeData(Request $request)
     {
         $searchValue = $request->input('search.value');
-        $orderColumnIndex = $request->input('order.1.column');
-        $orderDirection = $request->input('order.1.dir', 'asc');
+        $orderColumnIndex = $request->input('order.0.column');
+        $orderDirection = $request->input('order.0.dir', 'asc');
         $columns = $request->input('columns');
 
         $orderColumn = 'id';
@@ -27,8 +27,15 @@ class MCourseTypeController extends Controller
             $orderColumn = $columns[$orderColumnIndex]['data'];
         }
 
+        $orderColumnMapping = [
+            'DT_RowIndex' => 'id',
+        ];
+        
+        // Gunakan mapping untuk menentukan kolom pengurutan
+        $finalOrderColumn = $orderColumnMapping[$orderColumn] ?? $orderColumn;
+
         $courseTypes = MCourseType::select('id', 'name', 'slug', 'description', 'created_at', 'created_id', 'updated_at', 'updated_id', 'status')
-            ->orderBy($orderColumn, $orderDirection);
+            ->orderBy($finalOrderColumn, $orderDirection);
 
         // Filter kolom
         foreach ($columns as $column) {
