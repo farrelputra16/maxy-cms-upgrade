f9b531cc960508b806294773af468b4cce8abffb
william.ch1029 <>
2025-05-19 11:02:36 +0700

    feat: tracer study module (empty)

---
+++ Modules/TracerStudy/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('tracerstudy::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('tracerstudy.name') !!}
+    </p>
+@endsection
